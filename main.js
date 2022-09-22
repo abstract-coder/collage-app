cef4e37253e6f5ad64e5c5ce32c0af45ea62cb8b
ishita221209 <84296284+ishita221209@users.noreply.github.com>
2022-09-22 18:13:22 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Content;
+var recognition = new SpeechRecognition();
+
+function my_start()
+{
+    recognition.start();
+    document.getElementById("textbox").innerHTML= "";
+}
+
+recognition.onResults= function (event){
+    console.log(event);
+    var transcript = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML= transcript;
+    if(transcript == "selfie"){
+        speak();
+    }
+};
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width:500,
+    height:400,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+Webcam.attach(camera);
+
+function speak(){
+
+    
+    var synth = window.speechSynthesis;
+    Webcam.attach(camera);
+
+    speak_data = "Taking your next Selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    setTimeout(function(){
+        image_id= "selfie1";
+        take_snapshot();
+        speak_data= "Taking your selfie in 10 seconds";
+        utterThis= new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+}, 5000);
+
+    setTimeout(function(){
+        image_id= "selfie2";
+        take_snapshot();
+        speak_data= "Taking your selfie in 15 seconds"
+        utterThis= new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+    }, 10000)
+
+    setTimeout(function(){
+        image_id="selfie3";
+        take_snapshot();
+        speak_data= "Taking your selfie in 20 seconds";
+        utterThis= new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+    }, 15000);
+
+}
+
+function take_snapshot(){
+    console.log(image_id);
+    Webcam.snap(function(data_uri){
+        if (image_id == "selfie1"){
+            document.getElementById("results1").innerHTML= "<img src='"+data_uri+"'>"
+        }
+        if (image_id == "selfie2"){
+            document.getElementById("results2").innerHTML= "<img src='"+data_uri+"'>"
+        }
+        if (image_id == "selfie3"){
+            document.getElementById("results3").innerHTML= "<img src='"+data_uri+"'>"
+        }
+    });
+}
